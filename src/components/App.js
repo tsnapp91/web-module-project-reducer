6c5acb40f0decf38a86a43012b041e66e898323d
unknown <traviswsnapp@outlook.com>
2023-05-02 21:00:45 -0400

    clear button and operator button complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,22 @@ import TotalDisplay from "./TotalDisplay";
 import CalcButton from "./CalcButton";
 
 import reducer, { initialState } from "../reducers";
+import { applyNumber, changeOperator, clearDisplay } from "../actions";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleNumberClick = (number) => {
+    dispatch(applyNumber(number));
+  };
+
+  const handleOperatorClick = (operator) => {
+    dispatch(changeOperator(operator));
+  };
+
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
+  };
 
   return (
     <div className="App">
@@ -39,31 +52,43 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={1} onClick={() => handleNumberClick(1)} />
+              <CalcButton value={2} onClick={() => handleNumberClick(2)} />
+              <CalcButton value={3} onClick={() => handleNumberClick(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={() => handleNumberClick(4)} />
+              <CalcButton value={5} onClick={() => handleNumberClick(5)} />
+              <CalcButton value={6} onClick={() => handleNumberClick(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={() => handleNumberClick(7)} />
+              <CalcButton value={8} onClick={() => handleNumberClick(8)} />
+              <CalcButton value={9} onClick={() => handleNumberClick(9)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton
+                value={"+"}
+                onClick={() => handleOperatorClick("+")}
+              />
+              <CalcButton
+                value={"*"}
+                onClick={() => handleOperatorClick("*")}
+              />
+              <CalcButton
+                value={"-"}
+                onClick={() => handleOperatorClick("-")}
+              />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton
+                value={"CE"}
+                onClick={() => handleClearDisplay("CE")}
+              />
             </div>
           </form>
         </div>
